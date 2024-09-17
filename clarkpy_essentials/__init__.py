8ba25ed2caf5de1d859750bd1f4f8f6896ee5b04
clarkmaio <maioliandrea0@gmail.com>
2024-09-17 12:28:07 +0200

    node and pipeline in init

--- clarkpy_essentials/__init__.py
+++ clarkpy_essentials/__init__.py
@@ -3,3 +3,6 @@
 from .data_catalog.data_catalog import DataCatalog
 from .parser.parser_manager import ParserManager
 from .context.context import Context
+
+from .flow.node import Node
+from .flow.pipeline import Pipeline
